b3ead069c948380daabeabf947ba519570f913a3
iankang <ianngech@gmail.com>
2022-04-11 19:25:22 +0300

    initializing spring boot application

---
+++ src/test/java/com/queros/casestudy/CaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.queros.casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
